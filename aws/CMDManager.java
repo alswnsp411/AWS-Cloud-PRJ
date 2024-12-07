7f78b0e769b6b875845a3589ce3f27bce6622eed
alswnsp411 <alswnsp411@naver.com>
2024-12-07 21:28:28 +0900

    fix(InstanceStatus): SSH 접속이 활성화될때까지 대기하는 메서드 추가 - instance 실행 직후 SSH 접속 에러 방지

--- aws/CMDManager.java
+++ aws/CMDManager.java
@@ -4,14 +4,32 @@ import static config.configConstants.KEY_PATH;
 import static config.configConstants.USER;
 
 import com.jcraft.jsch.*;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 
 public class CMDManager {
 
+    private boolean isSSHAvailable(String instancePublicDNS) {
+        try (Socket socket = new Socket()) {
+            // 22번 포트로 연결 테스트 (5초 타임아웃)
+            socket.connect(new InetSocketAddress(instancePublicDNS, 22), 5000);
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
     public void runInstance(final String instancePublicDNS, final String command) {
         try {
+            while (!isSSHAvailable(instancePublicDNS)) {
+                System.out.println("SSH not available yet. Retrying...");
+                Thread.sleep(5000); // 5초 대기
+            }
+
             JSch jsch = new JSch();
             jsch.addIdentity(KEY_PATH);
             Session session = jsch.getSession(USER, instancePublicDNS, 22);
